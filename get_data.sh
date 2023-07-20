ae348d906ae1b9e4632d8ea4a9eeac9582cc1435
davbyr <dbyrne@woodwellclimate.org>
2023-07-20 19:50:36 +0000

    Removed get_data.sh

--- get_data.sh
+++
@@ -1,5 +0,0 @@
-# Make STORM directory in input
-mkdir input/STORM
-
-# Copy STORM track files from bucket to local environment
-gsutil -m cp gs://cmip5_data/tropical_cyclones/STORM/tracks/*$1*.txt ./input/STORM
