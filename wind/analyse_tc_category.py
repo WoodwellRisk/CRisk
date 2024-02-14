8a2dc10a1fadd67f2f2c62b3e1a3a39d864b7707
davbyr <dbyrne@woodwellclimate.org>
2024-02-14 20:39:01 +0000

    windspeed scripts updated

--- wind/analyse_tc_category.py
+++ wind/analyse_tc_category.py
@@ -8,120 +8,24 @@ import dask
 import dask.delayed as delayed
 from dask.distributed import Client
 from glob import glob
-import track_analysis as tana
-from track_analysis import radial_analysis
+from crisk.wind import radial_analysis
 import multiprocessing
 import rioxarray
+from climada.hazard import TCTracks
+from crisk import utils
+from paratc import track_tools, tctools
+import warnings
+import argparse
+from argparse import RawDescriptionHelpFormatter
 
-def main():
+if __name__ == '__main__':
 
-    margin = 3
-
-    # Loop over models in sepcified list
-    ds_list = []
-    
-    # List of models to analyse. Model data read as {dir_input}/{model_name}/*{basin}*
-    model_list = ['IBTRACS','CMCC','HADGEM','ECEARTH','CNRM']
-    
-    for model_name in model_list:
-        print(model_name)
-    
-        # Get list of inputs and read into dataframe
-        fp_inputs = glob(f'{dir_input}/{model_name}/*{basin}*')
-        tracks_list = [ tana.read_STORM( fp ) for fp in fp_inputs ]
-        tracks = pd.concat(tracks_list).reset_index()
-        print('    Tracks read.')
-    
-        # Separate events into years and randomly select n_years
-        tracks_years = tana.separate_years( tracks )
-        np.random.shuffle(tracks_years)
-        tracks = pd.concat(tracks_years[:n_years]).reset_index()
-    
-        # Separate tracks into individual events
-        tracks_events = tana.separate_events( tracks )
-        n_events = len(tracks_events)
-    
-        print('    Tracks separated into events.')
-    
-        client = Client()
-        
-        pdel = delayed(radial_analysis.multiple_analysis)
-        compute_list = []
-        batch_size = 2000
-        for idx0 in np.arange(0,n_events,batch_size):
-            idx1 = idx0 + batch_size
-            tracks_ii_list = tracks_events[idx0:idx1]
-            compute_list.append( pdel( tracks_ii_list, margin, 
-                                       lonmin, lonmax, latmin, latmax, 
-                                       resolution, radius, delta ) )
-        
-        out = dask.compute(compute_list, scheduler = 'processes')[0]
-
-        # Strip out processes where no storms passed
-        out = [i for i in out if i is not None]
-    
-        print('    Parallel work done.')
-        
-        ds_concat = xr.concat(out, dim='storm')
-        ds_total_count = ds_concat.sum(dim='storm').compute()
-        ds_count_per_year = ds_total_count / n_years
-    
-        attrs = {'title': f'Average number of tropical cyclones of varying categories passing within {radius}km per year.',
-                 'radius': f'{radius}km',
-                 'years_analysed': f'{n_years}',
-                 'track_dataset':'STORM',
-                 'model':f'{model_name}'}
-        ds_count_per_year.attrs = attrs
-        ds_list.append(ds_count_per_year)
-    
-        # Clean up
-        client.close()
-        del tracks
-        del tracks_list
-        del tracks_events
-
-    ds_ibtracs = ds_list[0]
-    ds_cmip = xr.concat(ds_list[1:], dim='model').mean(dim='model')
-    attrs = {'title': f'Average number of tropical cyclones of varying categories passing within {radius}km per year.',
-                 'radius': f'{radius}km',
-                 'years_analysed': f'{n_years}',
-                 'track_dataset':'STORM',
-                 'model':f'Model Mean'}
-    ds_cmip.attrs = attrs
-    ds_ibtracs = ds_ibtracs.rio.write_crs(4326)
-    ds_cmip = ds_cmip.rio.write_crs(4326)
-    
-    categories = [0, 1, 2, 3, 4, 5]
-    for cat in categories:
-        ds_cmip[f'category_{cat}'].rio.to_raster( os.path.join( dir_output, 
-                                   f'TC_annualprob_STORM_{basin}_{radius}km_cat{cat}_2015_2050.tif' ) )
-        ds_ibtracs[f'category_{cat}'].rio.to_raster( os.path.join( dir_output, 
-                                   f'TC_annualprob_STORM_{basin}_{radius}km_cat{cat}_1971_2000.tif' ) )
-    
-    # GREATER THAN DATASETS
-    for ii in range(5):
-        cc_list = categories[ii:]
-        cc = categories[ii]
-        ds_proj_ii = [ ds_cmip[f'category_{cc}'] for cc in cc_list]
-        ds_past_ii = [ ds_ibtracs[f'category_{cc}'] for cc in cc_list]
-    
-        ds_proj_ii = 1/xr.concat(ds_proj_ii, dim='category').sum(dim='category')
-        ds_proj_ii = ds_proj_ii.where( ds_proj_ii <= 200 ).rename(f'gt_category_{categories[ii]}')
-        ds_past_ii = 1/xr.concat(ds_past_ii, dim='category').sum(dim='category')
-        ds_past_ii = ds_past_ii.where( ds_past_ii <= 200 ).rename(f'gt_category_{categories[ii]}')
-    
-        ds_proj_ii.rio.to_raster( os.path.join( dir_output, f'TC_returnperiod_STORM_{basin}_{radius}km_gtcat{cc}_2015_2050.tif' ) )
-        ds_past_ii.rio.to_raster( os.path.join( dir_output, f'TC_returnperiod_STORM_{basin}_{radius}km_gtcat{cc}_1971_2000.tif' ) )
-
-
-if __name__ == "__main__":
-    '''
-    Calls main() after parsing input arguments.
-    '''
+    # Make client
+    client = Client()
 
     # Silence warnings
     warnings.filterwarnings("ignore")
-
+    
     description = ('')
                      
     # Parse input arguments
@@ -131,43 +35,95 @@ if __name__ == "__main__":
                     epilog='Author: David Byrne, Woodwell Climate Research Center',
                     formatter_class = RawDescriptionHelpFormatter)
     parser.add_argument('-basin', type=str, help='Name of STORM basin to use tracks from.', required=True,
-                        choices = ['EP','NA','NI','SI','SP','EP'])
-    parser.add_argument('-lat1', type=float, help='Minimum grid latitude')
-    parser.add_argument('-lat2', type=float, help='Maximum grid latitude')
-    parser.add_argument('-lon1', type=float, help='Minimum grid longitude')
-    parser.add_argument('-lon2', type=float, help='Maximum grid longitude')
+                        choices = ['EP','NA','NI','SI','SP','WP'])
+    parser.add_argument('-lon', type=float, help='Grid longitude (min, max)', nargs='+')
+    parser.add_argument('-lat', type=float, help='Grid latitude (min, max)', nargs='+')
     parser.add_argument('-res', type=float, help='Grid resolution in degrees [Default=0.05]', default=0.05)
-    parser.add_argument('-pt_lon', type=float, help='Longitude point locations', 
-                        default=None, nargs='+')
-    parser.add_argument('-pt_lat', type=float, help='Latitude point locations', 
-                        default=None, nargs='+')
-    parser.add_argument('-pt_name', type=str, help='Names of pt for output file [Default=None]', 
-                        default=None, nargs='+')
     parser.add_argument('-name', type=str, default='TEST',
                         help='Name of analysis run. Used in output filename. [Default=TEST]')
     parser.add_argument('-timestep', type=int, help='Timestep of tracks to use. Interpolates to finer track. [Default=1].',
-                        default=1)
-    parser.add_argument('-nyears', type=int, help='Number of synthetic years to use. [Default=10000].', default=10000)
+                        default=.25)
+    parser.add_argument('-nyears', type=int, help='Number of synthetic years to use. [Default=10000].', default=5000)
     parser.add_argument('-ncpus', type=int, default=1, 
                         help='Number of cpus to use in Pathos parallel processing [Default=1].')
-    parser.add_argument('-tracks_dir', type=str, default='./input/STORM',
+    parser.add_argument('-tracks_dir', type=str, default='../data/STORM',
                         help='Directory containing STORM concatenated files (10000 years) [Default=./input/STORM].')
-    parser.add_argument('-out_dir', type=str, help='Path to output directory [Default=./output]', default='./output')
-    parser.add_argument('-plot_dir', type=str, help='Path to plots directory [Default=./plots]', default='./plots')
+    parser.add_argument('-radius', type=str, help='Storm Radius', default=100)
+    parser.add_argument('-model', type=str, help='Track file names, or list of', nargs='+', default=None)
+    outdir = '.'
     args = parser.parse_args()
+    margin = 3
     
+    # Set default model names if not provided
+    if args.model is None:
+        model_list = ['IBTRACS','HADGEM','ECEARTH','CMCC','CNRM']
+    else:
+        model_list = args.model
+        
+    if type(model_list) is not list:
+        model_list = list(model_list)
     
-    # ANALYSIS SETTINGS
-    radius = 100
+    # Make the grid
+    grid_lon = np.arange( args.lon[0], args.lon[1], args.res )
+    grid_lat = np.arange( args.lat[0], args.lat[1], args.res )
+    lon2, lat2 = np.meshgrid( grid_lon, grid_lat )
+    grid_poly = utils.get_grid_poly( lon2, lat2 )
+
+    # Loop over models in sepcified list
+    ds_list = []
+    for model_name in model_list:
     
-    # Call main function
-    tick = time()
-    main(args.lat1, args.lat2, args.lon1, args.lon2, args.res, 
-         args.basin, args.timestep, args.nyears, args.ncpus, 
-         args.out_dir, args.tracks_dir, 
-         args.pt_lon, args.pt_lat, args.pt_name, 
-         args.plot_dir)
-    tock = time()
-    time_elapsed = (tock-tick)/60 #Minutes
-    print(' ')
-    print(f' Done. Total Walltime: {time_elapsed} minutes')
+        # Get list of inputs and read into dataframe
+        fp_in = f'{args.tracks_dir}/STORM_10000years_{model_name}_{args.basin}.txt'
+        print(f' Opening... {fp_in}')
+        tracks = TCTracks.from_simulations_storm( fp_in )
+        tracks.data = tracks.data
+        tracks.data = track_tools.shift_tracks_lon(tracks.data)
+        sid = [tr.sid for tr in tracks.data]
+        tracks = [utils.climada_to_dataframe(ds) for ds in tracks.data]
+
+        # Get years of STORM tracks
+        track_years = []
+        for ii, tr in enumerate(tracks):
+            track_years.append( float(sid[ii].split('-')[-2]) )
+        track_years = np.array(track_years)
+
+        # Take first n years of tracks
+        keep_idx = np.where(track_years < args.nyears)[0]
+        tracks = [tracks[ii] for ii in keep_idx]
+        sid = [sid[ii] for ii in keep_idx]
+        track_years = track_years[keep_idx]
+
+        # Remove 1 point tracks
+        tlens = np.array([len(tr) for tr in tracks])
+        keep_idx = np.where( tlens > 1 )[0]
+        tracks = [tracks[ii] for ii in keep_idx]
+        sid = [sid[ii] for ii in keep_idx]
+        track_years = track_years[keep_idx]
+    
+        # Remove tracks outside of the grid domain
+        keep_idx = track_tools.subset_tracks_in_poly(tracks, grid_poly, buffer=2 )
+        tracks = [tracks[ii] for ii in keep_idx]
+        sid = [sid[ii] for ii in keep_idx]
+        track_years = track_years[keep_idx]
+        print(f'    Subsetting to grid domain: {len(tracks)}', flush=True)
+
+        # Do the analysis for this model
+        ds_pasana = radial_analysis.passing_storm_stats( tracks, lon2, lat2, track_years,
+                                                         client=client, radius=args.radius)
+
+        # Format output and save to file
+        ds_pasana['annual_count'] = args.nyears / ds_pasana['annual_count']
+        ds_pasana['event_count'] = ds_pasana['event_count'] / args.nyears
+        ds_pasana = ds_pasana.rename({'annual_count':'return_period',
+                        'event_count':'events_per_year'})
+        ds_pasana.attrs = {'name':f'Hurricane frequencies within {args.radius}km according to the STORM dataset',
+                    'nyears':f'{args.nyears}',
+                    'model':f'{model_name}'}
+        ds_pasana.return_period.attrs = {'dscription':f'Annual return period of storm exceeding bin intensity'}
+        ds_pasana.events_per_year.attrs = {'description':f'Average number of events per year exceeding bin intensity'}
+        ds_pasana.bin.attrs = {'description':'Hurricane intensity bins',
+                        'units':'ms-1'}
+        ds_pasana['x'] = (['x'], grid_lon)
+        ds_pasana['y'] = (['y'], grid_lat)
+        ds_pasana.to_netcdf(f'passing_hurricanes_{args.name}_{model_name}_{args.radius}km.nc')
--- wind/analyse_tc_windspeed.py
+++ wind/analyse_tc_windspeed.py
@@ -5,187 +5,137 @@ from pathos.pools import ProcessPool as Pool
 import numpy as np
 import matplotlib.pyplot as plt
 import xarray as xr
-from tc_analysis import windspeed_analysis
+from crisk.wind import windspeed_analysis
 import os
 import argparse
 from argparse import RawDescriptionHelpFormatter
 import warnings
 from time import time
-from tc_analysis import plot as tcplot
 
+# Silence warnings
+warnings.filterwarnings("ignore")
 
-def main(min_lat, max_lat, min_lon, max_lon, resolution, 
-         basin, timestep, n_years,
-         ncpus, rperiods, out_dir, tracks_dir,
-         pt_lon, pt_lat, pt_name, plot_dir):
-    '''
-    MAIN function for windspeed_return_periods.py. Makes use of windspeed_analysis.tracks_to_wspd_rp()
-    and cliamda TCTracks() and Tropical_Cyclone() classes to analyse windspeeds for each of the 5
-    STORM models. Arguments are comparable to those in script header.
-    '''
+description = ('Use STORM synthetic tracks to calculate wind speed return periods associated with Tropical Cyclones.\n'
+              'You can generate these return periods on a regular grid or at set point locations.\n'
+              'Analysis will be saved to a netCDF file with name template tc_wpsd_returnlevels_grid_{name}_{nyears}years.nc. \n'
+              'To generate on a grid, you must specify at least: lat1, lat2, lon1, lon2.\n'
+              'To generate at a set of locations, you must at least provide pt_lon and pt_lat.\n'
+              'If you provide both grid and points, only the points will be analysed.\n'
+              'See arguments and options below for more information.\n\n'
+              'Example Usage: \n \n'
+              '# Generate windspeeds for pt at New York and Boston \n'
+              'python ./analyse_tc_windspeed.py -pt_lon -74 -71.1 -pt_lat 40.71 42.4'
+                   ' -pt_name new_york boston -ncpus 12 -name NYBO -basin NA\n\n'
+              '# Generate windspeeds for a grid around New York \n'
+              'python ./analyse_tc_windspeed.py -lat1 40 -lat2 41.5 -lon1 -75 -lon2 -71'
+                   ' -ncpus 12 -name NYBO -basin NA\n' )
+                 
+# Parse input arguments
+parser = argparse.ArgumentParser(
+                prog='analyse_tc_windspeed',
+                description= description,
+                epilog='Author: David Byrne, Woodwell Climate Research Center',
+                formatter_class = RawDescriptionHelpFormatter)
+parser.add_argument('-basin', type=str, help='Name of STORM basin to use tracks from.', required=True,
+                        choices = ['EP','NA','NI','SI','SP','WP'])
+parser.add_argument('-lon', type=float, help='Grid longitude (min, max)', nargs='+')
+parser.add_argument('-lat', type=float, help='Grid latitude (min, max)', nargs='+')
+parser.add_argument('-res', type=float, help='Grid resolution in degrees [Default=0.05]', default=0.05)
+parser.add_argument('-name', type=str, default='TEST',
+                    help='Name of analysis run. Used in output filename. [Default=TEST]')
+parser.add_argument('-timestep', type=int, help='Timestep of tracks to use. Interpolates to finer track. [Default=1].',
+                    default=1)
+parser.add_argument('-rperiods', nargs = '+', type=int,
+                    help='Return periods to analyse at [Default=5 10 100 500].',
+                    default=[5, 10, 100, 200, 500])
+parser.add_argument('-nyears', type=int, help='Number of synthetic years to use. [Default=10000].', default=10000)
+parser.add_argument('-ncpus', type=int, default=1, 
+                    help='Number of cpus to use in Pathos parallel processing [Default=1].')
+parser.add_argument('-tracks_dir', type=str, default='../data/STORM',
+                    help='Directory containing STORM concatenated files (10000 years) [Default=./input/STORM].')
+parser.add_argument('-model', type=str, help='Track file names, or list of', nargs='+', default=None)
+args = parser.parse_args()
 
-    # Define model names for analysis and file finding
-    model_names = ['IBTRACS','HADGEM','ECEARTH','CNRM','CMCC']
+outdir = '.'
+# Print some things 
+print(' ')
+print(f' *** Starting analysis with {args.nyears} years of simulated storms ***')
+print(f' Analyzing on grid with resolution {args.res} degrees:')
 
-    # Create a parallel pool to pass to climada functions
-    pool=Pool(ncpus=ncpus)
+print(' ******')
+print(' ')
 
-    # Make centroids and calculate distance to coast
-    print('Creating centroids and related parameters..')
-    if pt_lon is None or pt_lat is None:
-        gridded = True
-        cent = Centroids.from_pnt_bounds((min_lon, min_lat, max_lon, max_lat), res=resolution)
-    else:
-        gridded = False
-        cent = Centroids( np.array(pt_lat), np.array(pt_lon) )
-
-    # Calculate distance to land
-    cent.set_dist_coast( precomputed=True, signed = False)
-    #cent.set_on_land()
-    n_cent = len(cent.lon)
-    print(f'Total number of centroids: {n_cent} \n')
-
-    # Do return period analysis for each model individually
-    ds_out_list = []
-    for model in model_names:
-        print(f'Analyzing model: {model}')
-        
-        # Create tracks file path for this loop
-        fp_tracks = os.path.join( args.tracks_dir, f'STORM_10000years_{model}_{basin}.txt')
-        
-        # Read tracks from files and interpolate
-        print('   > Reading Tracks')
-        tracks = TCTracks.from_simulations_storm( fp_tracks, years=np.arange(0, n_years))
-        print('   > Interpolating Tracks')
-        tracks.equal_timestep(time_step_h = timestep, pool=pool)
-
-        # Do windspeed analysis
-        print('   > Expanding tracks into wind envelopes.')
-        ds_rp = windspeed_analysis.tracks_to_wspd_rp( tracks, cent, pool, n_years,
-                                                      rperiods = rperiods, reshape_2d = gridded)
-        ds_out_list.append(ds_rp)
-        del tracks
-
-    # Make output file name
-    fp_out = os.path.join(args.out_dir, f'tc_wpsd_returnlevels_{args.nyears}years_{args.name}.nc')
-
-    # Concatenate output datasets
-    ds_out = xr.concat(ds_out_list, dim='model')
-    ds_future = ds_out.return_level.isel(model=[1,2,3,4])
-    ds_out['wspd_ibtracs'] = ds_out.return_level.isel(model=0)
-    ds_out['wspd_mean'] = ds_future.median(dim='model')
-    ds_out['wspd_median'] = ds_future.mean(dim='model')
-    ds_out['wspd_std'] = ds_future.std(dim='model')
-    ds_out = ds_out.drop('return_level')
-
-    # Set point names if provided
-    if pt_name is not None:
-        ds_rp['loc'] = (['loc'], pt_name)
-
-    # Save output dataset to file
-    print(f'Saving to file: {fp_out}')
-    if os.path.exists(fp_out):
-        os.remove(fp_out)
-    ds_out.to_netcdf(fp_out)
-
-    # Make plots
-    print(f'Saving figures to {plot_dir}')
-    if gridded:
-        for rp in rperiods:
-            f,a = tcplot.compare_windspeed_grid( ds_out.wspd_ibtracs.sel(return_period = rp),
-                                                 ds_out.wspd_median.sel(return_period=rp) )
-            fp_fig = os.path.join( plot_dir, f'windspeed_grid_{rp}year_{args.name}.png')
-            plt.savefig( fp_fig, bbox_inches='tight', dpi=300 )
-
-def check_args(args):
-    if args.lat2 <= args.lat1:
-        raise ValueError(' lat2 must be greater than or equal to lat1.')
-    if args.lon2 <= args.lon1:
-        raise ValueError(' lon2 must be greater than or equal to lon1.')
+# Set default model names if not provided
+if args.model is None:
+    model_list = ['IBTRACS','HADGEM','ECEARTH','CMCC','CNRM']
+else:
+    model_list = args.model
     
+if type(model_list) is not list:
+    model_list = list(model_list)
 
-if __name__ == "__main__":
-    '''
-    Calls main() after parsing input arguments.
-    '''
+# Create a parallel pool to pass to climada functions
+pool=Pool(ncpus=args.ncpus)
 
-    # Silence warnings
-    warnings.filterwarnings("ignore")
+# Make centroids and calculate distance to coast
+print('Creating centroids and related parameters..')
+min_lon = args.lon[0]
+max_lon = args.lon[1]
+min_lat = args.lat[0]
+max_lat = args.lat[1]
+gridded = True
+cent = Centroids.from_pnt_bounds((min_lon, min_lat, max_lon, max_lat), res=args.res)
 
-    description = ('Use STORM synthetic tracks to calculate wind speed return periods associated with Tropical Cyclones.\n'
-                  'You can generate these return periods on a regular grid or at set point locations.\n'
-                  'Analysis will be saved to a netCDF file with name template tc_wpsd_returnlevels_grid_{name}_{nyears}years.nc. \n'
-                  'To generate on a grid, you must specify at least: lat1, lat2, lon1, lon2.\n'
-                  'To generate at a set of locations, you must at least provide pt_lon and pt_lat.\n'
-                  'If you provide both grid and points, only the points will be analysed.\n'
-                  'See arguments and options below for more information.\n\n'
-                  'Example Usage: \n \n'
-                  '# Generate windspeeds for pt at New York and Boston \n'
-                  'python ./analyse_tc_windspeed.py -pt_lon -74 -71.1 -pt_lat 40.71 42.4'
-                       ' -pt_name new_york boston -ncpus 12 -name NYBO -basin NA\n\n'
-                  '# Generate windspeeds for a grid around New York \n'
-                  'python ./analyse_tc_windspeed.py -lat1 40 -lat2 41.5 -lon1 -75 -lon2 -71'
-                       ' -ncpus 12 -name NYBO -basin NA\n' )
-                     
-    # Parse input arguments
-    parser = argparse.ArgumentParser(
-                    prog='analyse_tc_windspeed',
-                    description= description,
-                    epilog='Author: David Byrne, Woodwell Climate Research Center',
-                    formatter_class = RawDescriptionHelpFormatter)
-    parser.add_argument('-basin', type=str, help='Name of STORM basin to use tracks from.', required=True,
-                        choices = ['EP','NA','NI','SI','SP','EP', 'WP'])
-    parser.add_argument('-lat1', type=float, help='Minimum grid latitude')
-    parser.add_argument('-lat2', type=float, help='Maximum grid latitude')
-    parser.add_argument('-lon1', type=float, help='Minimum grid longitude')
-    parser.add_argument('-lon2', type=float, help='Maximum grid longitude')
-    parser.add_argument('-res', type=float, help='Grid resolution in degrees [Default=0.05]', default=0.05)
-    parser.add_argument('-pt_lon', type=float, help='Longitude point locations', 
-                        default=None, nargs='+')
-    parser.add_argument('-pt_lat', type=float, help='Latitude point locations', 
-                        default=None, nargs='+')
-    parser.add_argument('-pt_name', type=str, help='Names of pt for output file [Default=None]', 
-                        default=None, nargs='+')
-    parser.add_argument('-name', type=str, default='TEST',
-                        help='Name of analysis run. Used in output filename. [Default=TEST]')
-    parser.add_argument('-timestep', type=int, help='Timestep of tracks to use. Interpolates to finer track. [Default=1].',
-                        default=1)
-    parser.add_argument('-rperiods', nargs = '+', type=int,
-                        help='Return periods to analyse at [Default=5 10 100 500].',
-                        default=[5, 10, 100, 500])
-    parser.add_argument('-nyears', type=int, help='Number of synthetic years to use. [Default=10000].', default=10000)
-    parser.add_argument('-ncpus', type=int, default=1, 
-                        help='Number of cpus to use in Pathos parallel processing [Default=1].')
-    parser.add_argument('-tracks_dir', type=str, default='./input/STORM',
-                        help='Directory containing STORM concatenated files (10000 years) [Default=./input/STORM].')
-    parser.add_argument('-out_dir', type=str, help='Path to output directory [Default=./output]', default='./output')
-    parser.add_argument('-plot_dir', type=str, help='Path to plots directory [Default=./plots]', default='./plots')
-    args = parser.parse_args()
+# Calculate distance to land
+cent.set_dist_coast( precomputed=True, signed = False)
+#cent.set_on_land()
+n_cent = len(cent.lon)
+print(f'Total number of centroids: {n_cent} \n')
 
+# Do return period analysis for each model individually
+ds_out_list = []
+for model in model_list:
+    print(f'Analyzing model: {model}')
     
-    # Print some things 
-    print(' ')
-    print(f' *** Starting analysis with {args.nyears} years of simulated storms ***')
-    if args.pt_lon is None or args.pt_lat is None:
-        print(f' Analyzing on grid with resolution {args.res} degrees:')
-        print(f'    Latitude bounds: [{args.lat1}, {args.lat2}]')
-        print(f'    Longitude bounds: [{args.lon1}, {args.lon2}]')
-    else:
-        n_pt = len( args.pt_lon )
-        print(f' Analyzing for {n_pt} pt.')
-    print(' ******')
-    print(' ')
+    # Create tracks file path for this loop
+    fp_tracks = os.path.join( args.tracks_dir, f'STORM_10000years_{model}_{args.basin}.txt')
+    
+    # Read tracks from files and interpolate
+    print('   > Reading Tracks')
+    tracks = TCTracks.from_simulations_storm( fp_tracks, years=np.arange(0, args.nyears))
+    print('   > Interpolating Tracks')
+    tracks.equal_timestep(time_step_h = args.timestep, pool=pool)
 
-    # Check inputs
-    check_args(args)
-        
-    # Call main function
-    tick = time()
-    main(args.lat1, args.lat2, args.lon1, args.lon2, args.res, 
-         args.basin, args.timestep, args.nyears, args.ncpus, 
-         args.rperiods, args.out_dir, args.tracks_dir, 
-         args.pt_lon, args.pt_lat, args.pt_name, args.plot_dir)
-    tock = time()
-    time_elapsed = np.round((tock-tick)/60,2) #Minutes
-    print(' ')
-    print(f' Done. Total Walltime: {time_elapsed} minutes')
+    # Do windspeed analysis
+    print('   > Expanding tracks into wind envelopes.')
+    ds_rp = windspeed_analysis.tracks_to_wspd_rp( tracks, cent, pool, args.nyears,
+                                                  rperiods = args.rperiods, reshape_2d = gridded)
+    ds_out_list.append(ds_rp)
+    del tracks
+
+# Make output file name
+fp_out = os.path.join(outdir, f'tc_wpsd_returnlevels_{args.nyears}years_{args.name}.nc')
+
+# Concatenate output datasets
+ds_out = xr.concat(ds_out_list, dim='model')
+ds_future = ds_out.return_level.isel(model=[1,2,3,4])
+ds_out['wspd_ibtracs'] = ds_out.return_level.isel(model=0)
+ds_out['wspd_mean'] = ds_future.median(dim='model')
+ds_out['wspd_median'] = ds_future.mean(dim='model')
+ds_out['wspd_std'] = ds_future.std(dim='model')
+ds_out = ds_out.drop('return_level')
+
+# Save output dataset to file
+print(f'Saving to file: {fp_out}')
+if os.path.exists(fp_out):
+    os.remove(fp_out)
+ds_out.to_netcdf(fp_out)
+
+# # Make plots
+# print(f'Saving figures to {plot_dir}')
+# if gridded:
+#     for rp in rperiods:
+#         f,a = tcplot.compare_windspeed_grid( ds_out.wspd_ibtracs.sel(return_period = rp),
+#                                              ds_out.wspd_median.sel(return_period=rp) )
+#         fp_fig = os.path.join( plot_dir, f'windspeed_grid_{rp}year_{args.name}.png')
+#         plt.savefig( fp_fig, bbox_inches='tight', dpi=300 )
